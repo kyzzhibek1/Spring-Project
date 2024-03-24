dbe1cccfebdeeb34855e8158991f603f30de9465
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2024-03-24 13:14:13 +0600

    Add Base Entity

---
+++ src/main/java/com/example/entity/Order.java
@@ -0,0 +1,42 @@
+package com.example.entity;
+
+import com.example.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime orderTime;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_dish",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "dish_id")
+    )
+    private List<Dish> dishes;
+
+    private double totalCost;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
---
+++ src/main/java/com/example/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.example.enums;
+
+public enum OrderStatus {
+    IN_PROGRESS,
+    COMPLETED,
+    DELIVERED,
+    CANCELLED
+}
