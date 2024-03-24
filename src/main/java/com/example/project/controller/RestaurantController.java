a22b91cfaa8ef66b47e9064531c0a22ee2ad9645
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2024-03-24 19:46:35 +0600

    Add swagger to restaurant controller

--- src/main/java/com/example/project/controller/RestaurantController.java
+++ src/main/java/com/example/project/controller/RestaurantController.java
@@ -2,6 +2,8 @@ package com.example.project.controller;
 
 import com.example.project.dto.RestaurantDTO;
 import com.example.project.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,42 +14,53 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/restaurant")
+@Tag(
+        name = "Restaurant Controller",
+        description = "Controller for operations with restaurants"
+)
 public class RestaurantController {
 
     @Autowired
     private RestaurantService restaurantService;
 
     @GetMapping
+    @Operation(summary = "Getting page of restaurants")
     public ResponseEntity<Page<RestaurantDTO>> getAllRestaurants(@PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<RestaurantDTO> restaurants = restaurantService.getAllRestaurants(pageable);
         return ResponseEntity.ok(restaurants);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Getting restaurant by id")
     public ResponseEntity<RestaurantDTO> getRestaurantById(@PathVariable("id") Long id) {
         RestaurantDTO restaurant = restaurantService.getRestaurantById(id);
         return ResponseEntity.ok(restaurant);
     }
 
     @PostMapping
+    @Operation(summary = "Creating restaurant")
     public ResponseEntity<RestaurantDTO> createRestaurant(@Validated @RequestBody RestaurantDTO restaurantDto) {
         RestaurantDTO restaurant = restaurantService.createRestaurant(restaurantDto);
         return ResponseEntity.ok(restaurant);
     }
-
+    
+    
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleting restaurant by id")
     public ResponseEntity<Void> deleteRestaurant(@PathVariable("id") Long id) {
         restaurantService.deleteRestaurant(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{restaurant-id}/add-dish/{dish-id}")
+    @Operation(summary = "Adds dish to restaurant by their id")
     public ResponseEntity<RestaurantDTO> addDishToMenu(@PathVariable("restaurant-id") Long restaurantId, @PathVariable("dish-id") Long dishId) {
         RestaurantDTO restaurantDTO = restaurantService.addDishToMenu(restaurantId, dishId);
         return ResponseEntity.ok(restaurantDTO);
     }
 
     @DeleteMapping("/{restaurant-id}/delete-dish/{dish-id}")
+    @Operation(summary = "Deletes dish to restaurant by their id")
     public ResponseEntity<Void> deleteDishFromMenu(@PathVariable("restaurant-id") Long restaurantId, @PathVariable("dish-id") Long dishId) {
         restaurantService.deleteDishFromMenu(restaurantId, dishId);
         return ResponseEntity.noContent().build();
