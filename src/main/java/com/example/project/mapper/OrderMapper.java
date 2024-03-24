b3400b490f2441da2e585eaace9f16c140bee195
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2024-03-24 19:31:40 +0600

    Update OrderMapper for avoiding recursion

--- src/main/java/com/example/project/mapper/OrderMapper.java
+++ src/main/java/com/example/project/mapper/OrderMapper.java
@@ -3,10 +3,13 @@ package com.example.project.mapper;
 import com.example.project.dto.OrderDTO;
 import com.example.project.entity.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+    @Mapping(target = "user.orders", ignore = true)
     Order orderDtoToOrder(OrderDTO orderDTO);
 
+    @Mapping(target = "user.orders", ignore = true)
     OrderDTO orderToOrderDto(Order order);
 }
