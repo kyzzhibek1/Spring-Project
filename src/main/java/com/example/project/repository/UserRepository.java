3b933bbf6ff5010b3a860dd5ac4e382f99126b8f
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2024-05-12 22:55:53 +0600

    updated UserRepository.java

--- src/main/java/com/example/project/repository/UserRepository.java
+++ src/main/java/com/example/project/repository/UserRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Page<User> findAll(Pageable pageable);
+
+    Optional<User> findByPhoneNumber(String username);
+
 }
