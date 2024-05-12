7d5d2bdbe937aa843b3a638110596b4930882e07
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2024-05-12 22:54:58 +0600

    updated UserMapper.java

--- src/main/java/com/example/project/mapper/UserMapper.java
+++ src/main/java/com/example/project/mapper/UserMapper.java
@@ -1,12 +1,20 @@
 package com.example.project.mapper;
 
+import com.example.project.dto.AuthRegisterDTO;
 import com.example.project.dto.UserDTO;
 import com.example.project.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "authorities", ignore = true)
     User userDtoToUser(UserDTO userDTO);
 
     UserDTO userToUserDto(User user);
+
+    @Mapping(target = "authorities", ignore = true)
+    User authDtoToUser(AuthRegisterDTO authRegisterDTO);
+
 }
